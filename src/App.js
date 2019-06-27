f0704daf4b786e6cf8eb7d70d0795523d39ed5e4
Jennifer Boultareau <jennifer.boultareau@gmail.com>
2019-06-27 16:32:56 +0200

    home is ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import Home from './Components/Home/Home';
+import Summary from './Components/Summary/Summary';
+import { Route } from 'react-router-dom';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import M from '../node_modules/materialize-css/dist/js/materialize.js'; 
 
 function App() {
   return (
     <div className="App">
+    <Route path="/house" exact component={Summary} />
+    <Route path="/" exact component={Home} />
     </div>
   );
 }
