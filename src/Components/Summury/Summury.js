1b6934dec7d041ab5a43c111b02c6e64af78259f
Maelenn <ms44118@gmail.com>
2019-06-27 12:25:06 +0200

    summary.js init

---
+++ src/Components/Summury/Summury.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import salon from '../../assets/img/summary/salon.jpg';
+
+const Summary = () => {
+  <img src={salon} alt=''></img>
+  <img src='' alt=''></img>
+  <img src='' alt=''></img>
+  <img src='' alt=''></img>
+  <img src='' alt=''></img>
+  <img src='' alt=''></img>
+};
+
+export default Summary;
