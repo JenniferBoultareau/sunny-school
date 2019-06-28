4ddf6b9a07ab561921117cc2679428aa99a68f63
rachel-os <rachel.otete@gmail.com>
2019-06-28 09:11:56 +0200

    layout on modal

--- src/Components/Bathroom/Bathroom.js
+++ src/Components/Bathroom/Bathroom.js
@@ -1,43 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Bathroom.css';
 import { Modal, RadioGroup } from 'react-materialize';
 import Button from 'react-materialize/lib/Button';
 
+// const [result, setResult] = useState();
 
 export default function Bathroom() {
   return (
     <div className='hero'>
       <div className='bathroom'>
       <Modal 
-        header="La question sur la baignoire ? Selectionne la bonne réponse" 
-        bottomSheet 
-        trigger={<a className='btn-floating btn-large pulse bathtub pink'>
-        <i className='material-icons'>control_point</i>
-        </a>}>
+        header="La question sur la baignoire ? Sélectionne la bonne réponse:" 
+        bottomSheet
+        actions={
+          <Button modal="close" flat >Fermer</Button>
+          }
+        trigger=
+          {<a className='btn-floating btn-large pulse bathtub pink'>
+          <i className='material-icons'>control_point</i>
+          </a>}>
           <RadioGroup
             radioClassNames	= 'bathtub-radio'
-            onChange=''
-            name='question '
-            withGap= 'true'
-            value='xl'
+            // onChange={onChange}
+            name='question'
+            withGap
+            value=''
             options={[
-              {label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',value: 'bonne réponse'},
-              {label: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',value: 'l'},
-              {label: ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',value: 'C'}
+              {label: 'Réponse A',value: 'correct'},
+              {label: 'Réponse B',value: 'false'},
+              {label: 'Réponse C',value: 'false'}
             ]}
           />
-        <Button className="btn waves-effect waves-light" type="submit" name="action">Submit
+        <Button 
+          className="btn waves-effect waves-light" 
+          type="submit" 
+          name="action"
+          >
+          Envoyer
           <i className="material-icons right">send</i>
         </Button>
       </Modal>
       <Modal 
-      header="La question sur le lavabo qui va te retourner le cerveau" 
-      bottomSheet 
-      trigger={<a className='btn-floating btn-large pulse lavabo pink'>
-      <i className='material-icons'>control_point</i>
-      </a>}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-      </Modal>
+        header="La question sur le lavabo ?" 
+        bottomSheet 
+        trigger=
+        {<a className='btn-floating btn-large pulse lavabo pink'>
+        <i className='material-icons'>control_point</i>
+        </a>}>
+        <RadioGroup
+            radioClassNames	= 'bathtub-radio'
+            // onChange={onChange}
+            name='question'
+            withGap
+            value=''
+            options={[
+              {label: 'Réponse A',value: 'correct'},
+              {label: 'Réponse B',value: 'false'},
+              {label: 'Réponse C',value: 'false'}
+            ]}
+          />
+        <Button 
+          className="btn waves-effect waves-light" 
+          type="submit" 
+          name="action"
+          >
+          Envoyer
+          <i className="material-icons right">send</i>
+        </Button>      
+        </Modal>
       </div>
     </div>
   )
