15ecfb8a8781c757b2de343d61260c6b1aceee51
rachel-os <rachel.otete@gmail.com>
2019-06-28 11:33:00 +0200

    bad and good answer in radiogroup

--- src/Components/Bathroom/Bathroom.js
+++ src/Components/Bathroom/Bathroom.js
@@ -3,9 +3,18 @@ import './Bathroom.css';
 import { Modal, RadioGroup } from 'react-materialize';
 import Button from 'react-materialize/lib/Button';
 
-// const [result, setResult] = useState();
+
+
 
 export default function Bathroom() {
+  const [response, setResponse] = useState();
+  const [phlo, setPhlo] = useState(false);
+
+  const multipleAction = () => {
+    setPhlo(false);
+    setResponse()
+  }
+
   return (
     <div className='hero'>
       <div className='bathroom'>
@@ -13,28 +22,41 @@ export default function Bathroom() {
         header="La question sur la baignoire ? Sélectionne la bonne réponse:" 
         bottomSheet
         actions={
-          <Button modal="close" flat >Fermer</Button>
+          <Button onClick= {multipleAction
+          
+          } modal="close" flat >Fermer</Button>
           }
+          options = {
+            {dismissible:false
+        }
+        }
+
+      
         trigger=
           {<a className='btn-floating btn-large pulse bathtub pink'>
-          <i className='material-icons'>control_point</i>
+            <i className='material-icons'>control_point</i>
           </a>}>
-          <RadioGroup
-            radioClassNames	= 'bathtub-radio'
-            // onChange={onChange}
-            name='question'
-            withGap
-            value=''
-            options={[
-              {label: 'Réponse A',value: 'correct'},
-              {label: 'Réponse B',value: 'false'},
-              {label: 'Réponse C',value: 'false'}
-            ]}
-          />
+          { !phlo ? 
+                < RadioGroup
+                radioClassNames	= 'bathtub-radio'
+                onChange={(event) => setResponse(event.target.value) }
+                name='question'
+                withGap
+                value={response}
+                options={[
+                  {label: 'Réponse A',value: 'false'},
+                  {label: 'Réponse B',value: 'correct'},
+                  {label: 'Réponse C',value: 'false'}
+                ]}
+              />
+          :
+            (response === 'correct') ? <div>'Bonne réponse'</div>:<div>'Mauvaise réponse'</div>
+        }
         <Button 
           className="btn waves-effect waves-light" 
           type="submit" 
           name="action"
+          onClick = {() => setPhlo(true)}
           >
           Envoyer
           <i className="material-icons right">send</i>
