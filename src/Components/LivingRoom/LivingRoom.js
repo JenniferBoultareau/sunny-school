d85e84e2cd90f62e49192ce55bbc90dc94a8854d
rachel-os <rachel.otete@gmail.com>
2019-06-28 14:03:54 +0200

    salon

--- src/Components/LivingRoom/LivingRoom.js
+++ src/Components/LivingRoom/LivingRoom.js
@@ -4,6 +4,11 @@ import { Row, Container, Modal, Button, RadioGroup } from 'react-materialize';
 import './LivingRoom.css';
 
 function LivingRoom() {
+  const [responseTV, setResponseTV] = useState();
+  const [responselamp, setResponselamp] = useState();
+  const [answerTV, setAnswerTV] = useState(false);
+  const [answerlamp, setAnswerlamp] = useState(false);
+
 
   return (
     <div className='bg-image-livingroom'>
@@ -11,29 +16,97 @@ function LivingRoom() {
         <Link to='/house'>
           <i class="medium material-icons">arrow_back</i>
         </Link>
-          <Modal
-            header="La question sur la TV"
-            bottomSheet
-            trigger={<a className="btn-floating btn-large purple accent-3 pulse button-livingroom1"><i className="material-icons">wb_sunny</i></a>}
-          >
-            <RadioGroup
-              name="size"
+        <Modal className='TV'
+        header="Trouve le verbe dans la phrase suivante : Hier en quittant la maison, nous avons éteint la télé." 
+        bottomSheet
+        actions={
+          <Button  modal="close" flat >Fermer</Button>
+          }
+          options={
+            {dismissible:true,
+              onCloseEnd: function(modal, trigger) {
+                setAnswerTV(false);
+                setResponseTV()
+            }
+            }
+          }
+        trigger=
+          {<a className='btn-floating btn-large pulse button-livingroom1 purple accent-3'>
+            <i className='material-icons'>wb_sunny</i>
+          </a>}>
+          { !answerTV ? 
+              < RadioGroup
+              radioClassNames	= 'TV-radio'
+              onChange={(event) => setResponseTV(event.target.value) }
+              name='question'
               withGap
-              label="T-Shirt Size"
-              value="xl"
-              options={[{ label: 'Réponse A', value: 'xl' }, { label: 'Réponse2', value: 'l' }, { label: 'Réponse3', value: 'm' }]}
-            />
-            <Button
-              className='btn waves-effect waves-light'
-              type='submit'
-              name='action'
-            /*onChange={() => }*/>
-              ok
-            </Button>
-          </Modal>
-          <Modal header="Modal Header" bottomSheet trigger={<a className="btn-floating btn-large purple accent-3 pulse button-livingroom2"><i class="material-icons">wb_sunny</i></a>}>
-            bla
-          </Modal>
+              value={responseTV}
+              options={[
+                {label: "quittant",value: 'false'},
+                {label: 'avons éteint',value: 'correct'},
+                {label: 'éteint', value: 'false'}
+              ]}
+              />
+            : (responseTV === 'correct') ? 
+              <div className ='solution'>"Bonne réponse.  Pour moins consommer d’électricité, il est conseillé d’éteindre complètement les appareils électroniques au lieu de les laisser en veille."</div>
+              :<div className ='solution'>Dommage.. mauvaise réponse. Pour moins consommer d’électricité, il est conseillé d’éteindre complètement les appareils électroniques au lieu de les laisser en veille.</div>
+          }
+          <Button 
+            className="btn waves-effect waves-light" 
+            type="submit" 
+            name="action"
+            onClick = {() => setAnswerTV(true)}
+            >
+            Envoyer
+            <i className="material-icons right">send</i>
+          </Button>
+      </Modal>
+      <Modal className='lamp'
+        header="A quelle date l’homme a-t-il pu pour la première fois produire de l’électricité avec une cellule solaire ?" 
+        bottomSheet
+        actions={
+          <Button  modal="close" flat >Fermer</Button>
+          }
+          options={
+            {dismissible:true,
+              onCloseEnd: function(modal, trigger) {
+                setAnswerlamp(false);
+                setResponselamp()
+            }
+            }
+          }
+        trigger=
+          {<a className='btn-floating btn-large pulse button-livingroom2 purple accent-3'>
+            <i className='material-icons'>wb_sunny</i>
+          </a>}>
+          { !answerlamp ? 
+              < RadioGroup
+              radioClassNames	= 'lamp-radio'
+              onChange={(event) => setResponselamp(event.target.value) }
+              name='question'
+              withGap
+              value={responselamp}
+              options={[
+                {label: "1949",value: 'false'},
+                {label: '1887',value: 'false'},
+                {label: '1916', value: 'correct'}
+              ]}
+              />
+            : (responselamp === 'correct') ? 
+              <div className ='solution'>"Bonne réponse.</div>
+              :<div className ='solution'>Dommage... mauvaise réponse. </div>
+          }
+          <Button 
+            className="btn waves-effect waves-light" 
+            type="submit" 
+            name="action"
+            onClick = {() => setAnswerlamp(true)}
+            >
+            Envoyer
+            <i className="material-icons right">send</i>
+          </Button>
+      </Modal>
+
         </div>
     </div>
   )
